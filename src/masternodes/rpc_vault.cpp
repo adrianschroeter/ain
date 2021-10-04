8907cbfb50101621c49c2513329deafe2f107237
Diego del Corral Tercero <hi@dcorral.com>
2021-10-04 12:19:59 +0200

    isValid function for calculating validity of price ondemand + fix tests and reorganize

--- src/masternodes/rpc_vault.cpp
+++ src/masternodes/rpc_vault.cpp
@@ -76,7 +76,7 @@ bool IsVaultPriceValid(CCustomCSView& mnview, const CVaultId& vaultId, uint32_t 
             for(const auto collateral: collaterals->balances){
                 auto collateralToken = mnview.HasLoanSetCollateralToken({collateral.first, height});
                 if(auto fixedIntervalPrice = mnview.GetFixedIntervalPrice(collateralToken->fixedIntervalPriceId)){
-                    if (!fixedIntervalPrice.val->valid)
+                    if (!fixedIntervalPrice.val->isValid())
                         return false;
                 }
             }
@@ -85,7 +85,7 @@ bool IsVaultPriceValid(CCustomCSView& mnview, const CVaultId& vaultId, uint32_t 
             for(const auto loan: loans->balances){
                 auto loanToken = mnview.GetLoanSetLoanTokenByID(loan.first);
                 if(auto fixedIntervalPrice = mnview.GetFixedIntervalPrice(loanToken->fixedIntervalPriceId)){
-                    if (!fixedIntervalPrice.val->valid)
+                    if (!fixedIntervalPrice.val->isValid())
                         return false;
                 }
 
